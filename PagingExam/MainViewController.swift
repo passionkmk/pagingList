f7f7c99c408a92838a19f88009743c719c6bea9d
KimMinKu <passion.kmk@gmail.com>
2018-08-11 16:16:56 +0900

    Clean up Viewcontroller

--- PagingExam/MainViewController.swift
+++ PagingExam/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  PagingExam
 //
 //  Created by KimMinKu on 2018. 8. 11..
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+// MARK: - Overrides
+class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ 
+    }
 }
 
+// MARK: - Fucntions
+extension MainViewController {
+    
+}
